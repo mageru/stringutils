d5e20a674da0617e0bae02ecf8baf2bec2c71f4c
Justin Miller <jmiller9@gmail.com>
2022-01-13 12:52:31 -0500

    fixing return type

--- main.go
+++ main.go
@@ -4,12 +4,12 @@ import (
 	"strings"
 )
 
-func Upper(str string) {
-	var upper_str = strings.ToUpper(string)
+func Upper(str string) string {
+	var upper_str = strings.ToUpper(str)
 	return upper_str
 }
 
-func Lower(str string) {
-	var lower_str = strings.ToLower(string)
+func Lower(str string) string {
+	var lower_str = strings.ToLower(str)
 	return lower_str
 }
