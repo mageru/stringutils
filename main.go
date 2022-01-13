0b6d864de262fe151d6fb0a8054c4c3e42c3db52
Justin Miller <jmiller9@gmail.com>
2022-01-13 12:34:10 -0500

    learning go libs

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"strings"
+)
+
+func Upper(str string) {
+	var upper_str = strings.ToUpper(string)
+	return upper_str
+}
+
+func Lower(str string) {
+	var lower_str = strings.ToLower(string)
+	return lower_str
+}
